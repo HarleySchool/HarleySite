e248554b0148793d8921fc2e5d255cb2096ad738
Brandon <bwnance@gmail.com>
2014-09-02 02:15:35 -0400

    Second round of changes, more info, added PDFs for summer reading

--- js/templatemo_script.js
+++ js/templatemo_script.js
@@ -11,7 +11,6 @@ jQuery(function($) {
     });
     
     $(document).ready( function() {
-        loadGoogleMap();
         
         // backstretch for background image
         var defaultImgSrc = $('img.main-img').attr('src');
@@ -27,43 +26,7 @@ jQuery(function($) {
         $('#responsive').html(mainMenuList);
 	
 	//for image slide on menu item click(normal) and responsive
-	$("#menu-list a").on('mouseover',function(e){
-            e.preventDefault();
-            if (menuDisabled == false) // check the menu has disabled?
-            {
-                menuDisabled = true; // disable to menu
-                
-                var name = $(this).attr('href');
-                $('#menu-list li').removeClass('active');
-				
-                //  set active to both menu
-                var menuClass = $(this).parent('li').attr('class');
-                $('.'+menuClass).addClass('active');
-
-                // hide responsive menu
-                
-                // get image url and assign to backstretch for background
-                var imgSrc = $("img"+name+"-img").attr('src');
-                $.backstretch(imgSrc, {speed: 500}); //backstretch for background fade in/out
-                
-                // content slide in/out
-                $("section.active").animate({left:$("section.active").outerWidth()}, 400,function(){
-                    $(this).removeClass("active");
-                    $(this).hide();
-                    $(name+"-text").show();
-                    $(name+"-text").animate({left:'0px'},400,function(){
-                        $(this).addClass("active");
-                        
-                        google.maps.event.trigger(map, 'resize'); // resize map
-                        $.backstretch("resize"); // resize the background image
-                        
-                        menuDisabled = false; // enable the menu
-                    });
-                });
-                
-            }
-            return;
-	});
+	
         $("#menu-list a, #responsive a").on('click',function(e){
             e.preventDefault();
             if (menuDisabled == false) // check the menu has disabled?
@@ -93,7 +56,6 @@ jQuery(function($) {
                     $(name+"-text").animate({left:'0px'},400,function(){
                         $(this).addClass("active");
                         
-                        google.maps.event.trigger(map, 'resize'); // resize map
                         $.backstretch("resize"); // resize the background image
                         
                         menuDisabled = false; // enable the menu
@@ -106,22 +68,3 @@ jQuery(function($) {
     });
 
 });
-
-var map = '';
-
-function initialize() {
-    var mapOptions = {
-      zoom: 13,
-      center: new google.maps.LatLng(43.120189,-77.549766)
-    };
-    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
-}
-
-function loadGoogleMap(){
-    // load google map
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-        'callback=initialize';
-    document.body.appendChild(script);
-}
